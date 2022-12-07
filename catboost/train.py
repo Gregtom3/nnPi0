daf9a9bdb27264bfe4d0ff00ffb3fd2a67e532d8
Gregtom3 <gamatousek@gmail.com>
2022-12-07 12:57:00 -0500

    Cleanup

--- catboost/train.py
+++ catboost/train.py
@@ -46,39 +46,6 @@ def get_data():
             root_files.append(DATA_DIR+"/"+file+":PreProcessedEvents")
 
     print(len(root_files),"root files found for the ML train/test")
-    
-#     keys = ['flag',
-#             'nPhotons',
-#             'nHadrons',
-#              'gE',
-#              'gTheta',
-#              'gPhi',
-#              'g_pcal_e',
-#              'g1_pcal_e',
-#              'g2_pcal_e',
-#              'g_pcal_du',
-#              'g_pcal_dv',
-#              'g_pcal_m2u',
-#              'g_pcal_m2v',
-#              'g_pcal_m3u',
-#              'g_pcal_m3v',
-#              'g1R',
-#              'g2R',
-#              'g1M',
-#              'g2M',
-#              'g1dE',
-#              'g2dE',
-#              'h1R',
-#              'h2R',
-#              'h1M',
-#              'h2M',
-#              'h1dE',
-#              'h2dE',
-#              'h1q',
-#              'h2q',
-#              'eR',
-#              'eM',
-#              'edE']
 
     #load the tree
     tree = uproot.open(root_files[0])
