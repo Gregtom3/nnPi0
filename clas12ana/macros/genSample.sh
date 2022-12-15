31cea7dfda952ae596f0947868407db3b2f811dd
Gregtom3 <gamatousek@gmail.com>
2022-12-15 10:42:24 -0500

    Flags

--- clas12ana/macros/genSample.sh
+++ clas12ana/macros/genSample.sh
@@ -1,19 +1,101 @@
 #!/bin/bash
 USERNAME="$USER"
 hl="--------------------------------------------------------------------------------------------------------------------------------------------"
+
+if [ $# -lt 2 ]; then
+  echo """
+  USAGE: $0 [MC/nSidis] [ML Method] [flags(optional)]
+  Automates the generation of raw TTrees from the CLAS12 HIPO data
+  After the raw TTrees are created (or if they are created already) this program preprocesses
+  the raw TTrees into the input space of various ML algorithms
+   - [MC/nSidis]: Specifies if we are analyzing MC or nSidis data
+   - [ML Method]: Options
+              -catboost
+              -particleNet
+   - [flags]:
+              -o      (overwrite existing raw TTrees)
+              -nFiles <INT> (maximum number of files per hipo directory: default=5)
+              -noslurm (run all the processing code in command line without slurm)
+              -maxEvents <INT> (set the maximum number of events analyzed per hipo file)
+  """
+  exit 2
+fi
+
+PREPROCESSES="catboost particleNet"
+ANAS="MC nSidis"
+nCPUs=4
+memPerCPU=4000
+
+declare -A flags
+declare -A booleans
+args=()
+
+while [ "$1" ];
+do
+    arg=$1
+    if [ "${1:0:1}" == "-" ]
+    then
+      shift
+      rev=$(echo "$arg" | rev)
+      if [ -z "$1" ] || [ "${1:0:1}" == "-" ] || [ "${rev:0:1}" == ":" ]
+      then
+        bool=$(echo ${arg:1} | sed s/://g)
+        booleans[$bool]=true
+      else
+        value=$1
+        flags[${arg:1}]=$value
+        shift
+      fi
+    else
+      args+=("$arg")
+      shift
+    fi
+done
+
+#######################################################################################################
+
+ana=${args[0]}
+preprocess=${args[1]}
 nFiles=5
+
+if [ ! -z ${flags["nFiles"]} ]; then
+    nFiles=${flags["nFiles"]}
+fi
+
+if ! echo "$PREPROCESSES" | grep -q "$preprocess"; then
+    echo $hl
+    echo "ERROR: ML Method $preprocess not valid. Must use from following list [$PREPROCESSES]"
+    exit 2
+else
+    rungroup="${rungroup//-}"
+fi
+
+if ! echo "$ANAS" | grep -q "$ana"; then
+    echo $hl
+    echo "ERROR: Analysis type $ana not valid. Must use from following list [$ANAS]"
+    exit 2
+fi
+
 nEvents=1000000 # per file
-ana="nSidis" # either MC or nSidis
-preprocess="catboost" # catboost or particleNet
+if [ ! -z ${flags["maxEvents"]} ]; then
+    nEvents=${flags["maxEvents"]}
+fi
+
 volatiledir=/volatile/clas12/users/gmat/clas12analysis.sidis.data/rga/ML
 pwd=$PWD
 
 slurm=true
+if [ ! -z ${booleans["noslurm"]} ]; then
+    slurm=false
+fi
+
+
+
 logdir=""
 slurmdir=""
 if [ $slurm ]; then 
     
-        NOW=$( date '+%F_%H_%M' )
+        NOW=$( date '+%F_%H_%M_%S' )
         NOWdir=/farm_out/gmat/clas12analysis.sidis.data/rga/ML/$NOW
         if [ -d "${NOWdir}/" ]; then
             rm -r ${NOWdir}
@@ -30,6 +112,8 @@ if [ $slurm ]; then
         mkdir ${logdir}
         mkdir ${slurmdir}
 fi
+
+
 
 nCPUs=4
 memPerCPU=4000
@@ -86,8 +170,9 @@ do
 #SBATCH --error=${logdir}/${ana}_${runNumber}_${preprocess}.err
 $slurmshell
 EOF
-        
-        cat >> $slurmshell << EOF
+
+        if [ ! -z ${booleans["o"]} ]; then
+            cat >> $slurmshell << EOF
 #!/bin/tcsh
 source /group/clas12/packages/setup.csh
 module load clas12/pro
@@ -95,7 +180,19 @@ clas12root -b -q $PWD/pi0_readHipo.C\(\"${hipo}\",\"${raw_out}\",$beamE,$nEvents
 clas12root -b -q $PWD/pi0_preprocess_${preprocess}.C\(\"${raw_out}\",\"${preprocess_out}\"\)
 echo "Done"
 EOF
-        
+        else
+            cat >> $slurmshell << EOF
+#!/bin/tcsh
+source /group/clas12/packages/setup.csh
+module load clas12/pro
+if ( ! -e "${raw_out}" ) then
+    clas12root -b -q $PWD/pi0_readHipo.C\(\"${hipo}\",\"${raw_out}\",$beamE,$nEvents,$hipo_is_mc\)
+endif
+clas12root -b -q $PWD/pi0_preprocess_${preprocess}.C\(\"${raw_out}\",\"${preprocess_out}\"\)
+echo "Done"
+EOF
+	fi
+
         sbatch $slurmslurm
         
     else    
@@ -124,3 +221,8 @@ EOF
     
 done
 done
+
+
+echo $hl
+echo "Location of log files: $log_dir"
+echo "Location of slurm files: $slurm_dir"
